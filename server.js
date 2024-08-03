59a666f5b4d723880def030bdec4196153ebb999
Rohit-huta-p <vcrohithuta.p@gmail.com>
2024-08-03 19:00:29 +0530

    cors reset

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
-    origin: '*', // your frontend's domain
+    origin: 'https://danceclassdashboard.vercel.app',
     credentials: true,
 }));
 
